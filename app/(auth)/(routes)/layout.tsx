b221507e8bdee1dcd7f691c8b3d3193ed3040ed6
Your Name <email.ansh.on.com>
2025-04-02 19:42:07 -0700

    Finished another set of page routing

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { Children } from 'react'
+
+const AuthLayout = ({ children } : { children: React.ReactNode}) => {
+  return (
+    <div className="bg-blue-500 h-screen">{
+        children
+}</div>
+  )
+}
+
+export default AuthLayout
---
+++ app/(index)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils"
+import Image from "next/image";
+
+const styleCondition = true;
+export default function Home() {
+  return (
+    <div className="">
+      <p>Welcome to Cordis</p>
+      <Button className={cn("bg-red-500", styleCondition && "bg-slate-400")}>Click Me</Button>
+      </div>
+  );
+}
