cd3f05f900bf284d3f441ebaafcd9b85eed53d4f
Your Name <email.ansh.on.com>
2025-05-02 20:55:06 -0700

    Remove old home page component and add setup page with profile initialization and server redirection logic

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,26 @@
+import { db } from "@/lib/db";
+ import { initialProfile } from "@/lib/initial-profile";
+ import { Profile, Server } from "@prisma/client";
+ import { redirect } from "next/navigation";
+ import React from "react";
+ 
+ const SetupPage = async () => {
+   const profile: Profile | any = await initialProfile();
+ 
+   const server: Server | any = db.server.findFirst({
+     where: {
+       members: {
+         some: {
+           profileId: profile.id,
+         },
+       },
+     },
+   });
+ 
+   if (server) {
+     return redirect(`/servers/${server.id}`);
+   }
+   return <div>SetupPage</div>;
+ };
+ 
+ export default SetupPage;
