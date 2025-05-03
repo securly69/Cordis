2c861f425be26ec824caf5dd0eb45f5cf4e01b26
Your Name <email.ansh.on.com>
2025-05-02 21:33:17 -0700

    Add initial modal component for server customization, integrate form handling with Zod and React Hook Form, and update dependencies in package.json and package-lock.json

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,26 +1,27 @@
+import { InitialModal } from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
- import { initialProfile } from "@/lib/initial-profile";
- import { Profile, Server } from "@prisma/client";
- import { redirect } from "next/navigation";
- import React from "react";
- 
- const SetupPage = async () => {
-   const profile: Profile | any = await initialProfile();
- 
-   const server: Server | any = db.server.findFirst({
-     where: {
-       members: {
-         some: {
-           profileId: profile.id,
-         },
-       },
-     },
-   });
- 
-   if (server) {
-     return redirect(`/servers/${server.id}`);
-   }
-   return <div>SetupPage</div>;
- };
- 
- export default SetupPage;
+import { initialProfile } from "@/lib/initial-profile";
+import { Profile, Server } from "@prisma/client";
+import { redirect } from "next/navigation";
+import React from "react";
+
+const SetupPage = async () => {
+  const profile: Profile | any = await initialProfile();
+
+  const server: Server | any = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+  return <InitialModal />;
+};
+
+export default SetupPage;
